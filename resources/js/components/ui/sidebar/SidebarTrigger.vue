2d8bf732f6e07f219085cfc82e4ed7a9d633c882
endo1monir <andrew.monir@yahoo.com>
2025-11-23 10:09:26 +0200

    fix the sidebar RTL problem in the admin

--- resources/js/components/ui/sidebar/SidebarTrigger.vue
+++ resources/js/components/ui/sidebar/SidebarTrigger.vue
@@ -21,7 +21,7 @@ const { toggleSidebar } = useSidebar()
     :class="cn('h-7 w-7', props.class)"
     @click="toggleSidebar"
   >
-    <PanelLeft />
+    <PanelLeft class="rtl:scale-x-[-1]" />
     <span class="sr-only">Toggle Sidebar</span>
   </Button>
 </template>
